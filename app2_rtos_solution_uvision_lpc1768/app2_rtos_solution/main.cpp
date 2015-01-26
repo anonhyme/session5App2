ea8101f1b071a1135fc447b981cd81c7885ee371
anonhyme <antoine.bouchard2@usherbrooke.ca>
2015-01-26 14:20:58 -0500

    update nouvel facon

--- app2_rtos_solution_uvision_lpc1768/app2_rtos_solution/main.cpp
+++ app2_rtos_solution_uvision_lpc1768/app2_rtos_solution/main.cpp
@@ -6,54 +6,84 @@
 
 DigitalIn digitalIn1(p15);
 AnalogIn analogIn1(p19);
-
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
 Serial pc(USBTX, USBRX);
 
-void digital_signal(void const *args)
-{
+Mutex mutex;
+
+int data_digital=1;
+
+
+typedef struct {
+	int type;
+	time_t time_stamp;
+	int data;
+	int event;
+}Event;
+
+Mail<Event, 16> mail_box;
+
+// ** Traitement signal digital ** //
+
+void set_digital_signal(void const *args)
+	{
 	Thread* t = (Thread *) args;
 	t->signal_set(DIGITAL_SIGNAL);
 }
-
-void analog_signal(void const *args)
-{
-	Thread* t = (Thread *) args;
-	t->signal_set(ANALOG_SIGNAL);
-}
-
-void analog_read(void const *args)
-{
-	while (true)
-	{
-		// synchronisation sur la période d'échantillonnage
-		// lecture de l'étampe temporelle
-		// lecture des échantillons analogiques
-		// calcul de la nouvelle moyenne courante
-		// génération éventuelle d'un événement
-	}
-}
-
-typedef struct {
 	
-} digital_struct;
+void digital_sampling(void const *args) {
+	data_digital = digitalIn1.read();
+	set_digital_signal(args);
+}
 
 void digital_read(void const *args)
-{
-	int state = digitalIn1.read();
-	time_t stamp;
-	while (true)
 	{
-		// synchronisation sur la période d'échantillonnage (lecture aux 100ms, stabilisation 50ms)
+	// Sync at 100ms (stabilisation 50ms)
+	int state[2] = {0,0};
+	int i = 0;
+	time_t stamp;
+	while (true){
 		// Signal flags that are reported as event are automatically cleared.
     Thread::signal_wait(DIGITAL_SIGNAL);
+		i = i%2;
 		
+		if(i==0){
+			// store
+		} else {
+			// check data with last et si pareille enregistre 
+		}
+		
+		state[i] = digitalIn1.read();
+		i++;
 		// lecture de l'étampe temporelle 
 		stamp = time(NULL);
-		printf("HELLO ! Digital Read");
+		printf("First : %d    Second: %d                                               \r", state[0], state[1]);
 		// lecture des échantillons numériques
 		//if (state != digitalIn1.read())
 		//{}
 		// prise en charge du phénomène de rebond
+		// génération éventuelle d'un événement
+	}
+}
+
+// ** Traitement signal analog ** //
+void analog_sampling(void const *args) {
+	//analog.time_stamp = time(NULL);
+	//data_digital = analogIn1.read();
+}
+
+void analog_signal(void const *args){
+	Thread* t = (Thread *) args;
+	t->signal_set(ANALOG_SIGNAL);
+}
+
+void analog_read(void const *args){
+	while (true){
+		// synchronisation sur la période d'échantillonnage
+		// lecture de l'étampe temporelle
+		// lecture des échantillons analogiques
+		// calcul de la nouvelle moyenne courante
 		// génération éventuelle d'un événement
 	}
 }
@@ -68,25 +98,26 @@ void collection(void const *args)
 	}
 }
 
-int main()
-{
+void init() {
 	// initialisation du RTC
 	set_time(1422222222);
-	
+	// speed up serial
+	pc.baud(19200);
+	led1 = 0;
+}
+
+int main()
+{
+	init();
 	time_t seconds = time(NULL);
-  //printf("\rSeconds since January 1, 1970: %d           \n\r", seconds);
 	//printf("Time as a basic string = %s                 \n\r", ctime(&seconds));
 	
 	// Thread init.
 	Thread digital_thread(digital_read);
-	Thread analog_thread(analog_read);
+	//Thread analog_thread(analog_read);
 	
-	RtosTimer readNumTimer(digital_signal, osTimerPeriodic, &digital_thread);
-	RtosTimer readAnalTimer(analog_signal, osTimerPeriodic, &analog_thread);
-	
-	readNumTimer.start(200);
-	
-	while(1)
-	{
-	}
+	RtosTimer readNumTimer(digital_sampling, osTimerPeriodic, &digital_thread);
+	//RtosTimer readAnalTimer(analog_sampling, osTimerPeriodic);
+	readNumTimer.start(1000);
+	Thread::wait(osWaitForever);
 }
